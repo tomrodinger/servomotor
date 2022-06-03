02dd5482f54f11a0f97bcb4624b540bd78940ac9
Tomas Rodinger <tom@Tomass-MacBook-Pro-9.local>
2022-06-04 06:53:59 +0800

    Merging in the other branch where development occured for quite some time

--- Src/bootloader/Src/clock_calibration.h
+++
@@ -1,8 +0,0 @@
-#ifndef SRC_CLOCK_CALIBRATION_H_
-#define SRC_CLOCK_CALIBRATION_H_
-
-int32_t time_sync(uint64_t time_from_master);
-uint16_t get_clock_calibration_value(void);
-
-
-#endif /* SRC_CLOCK_CALIBRATION_H_ */
--- Src/bootloader/Src/mosfets.h
+++
@@ -1,9 +0,0 @@
-#ifndef MOSFETS_H_
-#define MOSFETS_H_
-
-
-void enable_mosfets(void);
-void disable_mosfets(void);
-uint8_t get_mosfets_enabled(void);
-
-#endif
